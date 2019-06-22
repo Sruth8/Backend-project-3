3074b0a9f3aa059b5c12dcf3aca1c03772e25000
Shelisha Rutherford <starshipazul@yahoo.com>
2019-06-21 21:49:07 -0400

    in progress

--- mongo/db.js
+++ mongo/db.js
@@ -7,8 +7,11 @@
 // ____log in - Firebase -ME
 // _____Saved 4. Sales -Mongo -ME
 // ____Post - Mongo - ME
-// ____comments on details Page - Mongo
 // ___ make sure the user can edit and delete stuff
+
+
+// ____comments on details Page - Mongo
+
 // ___start and stop of the yard sale. the time the sale is over
 // --- use password for the username.
 //___ adman panial 
